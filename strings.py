e68fa41185a3d480bfb613680c779ea209d473cb
Kaushik Ranjan <kaushikranjanrajkumar@gmail.com>
2024-05-03 11:18:29 +0530

    3 may 2024

---
+++ strings.py
@@ -0,0 +1,11 @@
+str="lorem ipsum dolor sit amet, consectetur adip!!"
+print(str.endswith("ip"))
+print(str.upper())
+print(str.isupper())
+print(str.isalnum())
+print(str.strip())
+print(str.rstrip("!")) 
+print(str.replace(" a", " e"))
+
+fruit = "apple" 
+print(fruit[0:3])
